cba1d7d36f2196f3bf11879e69cb12fca3ca1646
henrycoding <841636454@qq.com>
2024-12-29 01:13:51 +0800

    Update dumper.sh

--- dumper.sh
+++ dumper.sh
@@ -1025,6 +1025,7 @@ find "$OUTDIR" -type f -printf '%P\n' | sort | grep -v ".git/" > "$OUTDIR"/all_f
 if [[ "$treble_support" = true ]]; then
         aospdtout="lineage-device-tree"
         mkdir -p $aospdtout
+	cp $OUTDIR/boot/ramdisk/fstab* $OUTDIR/vendor/etc
         python3 -m aospdtgen $OUTDIR -o $aospdtout
 
         # Remove all .git directories from aospdtout
